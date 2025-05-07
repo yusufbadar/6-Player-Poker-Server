88e9a7c158aa436315f260c14043d74d6c9822b3
Yusuf Badar <ybadar13@gmail.com>
2025-05-07 03:12:05 +0000

    Test 1

--- src/server/game_logic.c
+++ src/server/game_logic.c
@@ -36,18 +36,49 @@ void shuffle_deck(card_t deck[DECK_SIZE]){ //DO NOT TOUCH THIS FUNCTION
 }
 
 //You dont need to use this if you dont want, but we did.
-void init_game_state(game_state_t *game, int starting_stack, int random_seed){
+void init_game_state(game_state_t *game, int starting_stack, int random_seed)
+{
     memset(game, 0, sizeof(game_state_t));
     init_deck(game->deck, random_seed);
-    for (int i = 0; i < MAX_PLAYERS; i++) {
+    shuffle_deck(game->deck);
+
+    game->next_card = 0;
+    game->round_stage = ROUND_JOIN;
+
+    for (int i = 0; i < MAX_PLAYERS; ++i) {
         game->player_stacks[i] = starting_stack;
+        game->player_status[i] = PLAYER_LEFT;
+        game->sockets[i] = -1;
+        for (int c = 0; c < HAND_SIZE; ++c)
+            game->player_hands[i][c] = NOCARD;
     }
+    for (int i = 0; i < MAX_COMMUNITY_CARDS; ++i)
+        game->community_cards[i] = NOCARD;
+
+    game->dealer_player = 0;
+    game->current_player = 1;
 }
 
-void reset_game_state(game_state_t *game) {
+void reset_game_state(game_state_t *game)
+{
     shuffle_deck(game->deck);
-    //Call this function between hands.
-    //You should add your own code, I just wanted to make sure the deck got shuffled.
+    game->next_card = 0;
+    game->round_stage = ROUND_INIT;
+    game->pot_size = 0;
+    game->highest_bet = 0;
+
+    game->dealer_player = (game->dealer_player + 1) % MAX_PLAYERS;
+    game->current_player = (game->dealer_player + 1) % MAX_PLAYERS;
+
+    for (int i = 0; i < MAX_COMMUNITY_CARDS; ++i)
+        game->community_cards[i] = NOCARD;
+    for (int p = 0; p < MAX_PLAYERS; ++p) {
+        game->current_bets[p] = 0;
+        if (game->player_status[p] != PLAYER_LEFT)
+            game->player_status[p] = PLAYER_ACTIVE;
+        for (int c = 0; c < HAND_SIZE; ++c)
+            game->player_hands[p][c] = NOCARD;
+    }
 }
 
 void server_join(game_state_t *game) {
@@ -62,30 +93,55 @@ int server_ready(game_state_t *game) {
 }
 
 //This was our dealing function with some of the code removed (I left the dealing so we have the same logic)
-void server_deal(game_state_t *game) {
-    for (int i = 0; i < MAX_PLAYERS; i++) {
-        if (game->player_status[i] == PLAYER_ACTIVE) {
-            game->player_hands[i][0] = game->deck[game->next_card++];
-            game->player_hands[i][1] = game->deck[game->next_card++];
+void server_deal(game_state_t *g)
+{
+    for (int seat = 0; seat < MAX_PLAYERS; ++seat) {
+        if (g->player_status[seat] == PLAYER_ACTIVE) {
+            g->player_hands[seat][0] = g->deck[g->next_card++];
+            g->player_hands[seat][1] = g->deck[g->next_card++];
         }
     }
+
+    g->round_stage  = ROUND_PREFLOP;
+    g->highest_bet  = 0;
+    memset(g->current_bets, 0, sizeof(g->current_bets));
 }
 
-int server_bet(game_state_t *game) {
-    //This was our function to determine if everyone has called or folded
-    (void) game;
-    return 0;
+int server_bet(game_state_t *g) { 
+    return check_betting_end(g); 
 }
 
 // Returns 1 if all bets are the same among active players
-int check_betting_end(game_state_t *game) {
-    (void)game;
-    return 0; 
+int check_betting_end(game_state_t *g)
+{
+    int ref = -1;
+    for (int i = 0; i < MAX_PLAYERS; ++i) {
+        if (g->player_status[i] == PLAYER_ACTIVE) {
+            if (ref == -1) ref = g->current_bets[i];
+            if (g->current_bets[i] != ref) return 0;
+        }
+    }
+    return 1;
 }
 
-void server_community(game_state_t *game) {
-    //This function checked the game state and dealt new community cards if needed;
-    (void) game;
+void server_community(game_state_t *g)
+{
+    switch (g->round_stage) {
+        case ROUND_PREFLOP:
+            for (int i = 0; i < 3; ++i) g->community_cards[i] = g->deck[g->next_card++];
+            g->round_stage = ROUND_FLOP; break;
+        case ROUND_FLOP:
+            g->community_cards[3] = g->deck[g->next_card++];
+            g->round_stage = ROUND_TURN; break;
+        case ROUND_TURN:
+            g->community_cards[4] = g->deck[g->next_card++];
+            g->round_stage = ROUND_RIVER; break;
+        default: break;
+    }
+
+    g->highest_bet = 0;
+    memset(g->current_bets, 0, sizeof(g->current_bets));
+    g->current_player = (g->dealer_player + 1) % MAX_PLAYERS;
 }
 
 void server_end(game_state_t *game) {
