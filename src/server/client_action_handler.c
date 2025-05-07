37550b8fdf80d30394d456889f5e3ea5edba5e84
Yusuf Badar <ybadar13@gmail.com>
2025-05-07 03:11:39 +0000

    Test1

--- src/server/client_action_handler.c
+++ src/server/client_action_handler.c
@@ -16,25 +16,118 @@
  * @param out Pointer to a server_packet_t that will be filled with the response.
  * @return 0 if successful processing, -1 on NACK or error.
  */
-int handle_client_action(game_state_t *game, player_id_t pid, const client_packet_t *in, server_packet_t *out) {
-    //Optional function, see documentation above. Strongly reccomended.
-    (void)game;
-    (void)pid;
-    (void)in;
-    (void)out;
-    return -1;
+static int to_packet_status(player_status_t s)
+{
+    switch (s) {
+        case PLAYER_FOLDED: return 0;
+        case PLAYER_ACTIVE:
+        case PLAYER_ALLIN: return 1;
+        case PLAYER_LEFT: return 2;
+        default: return 2;
+    }
 }
 
-void build_info_packet(game_state_t *game, player_id_t pid, server_packet_t *out) {
-    //Put state info from "game" (for player pid) into packet "out"
-    (void) game;
-    (void) pid;
-    (void) out;
+int handle_client_action(game_state_t *g, player_id_t pid,
+    const client_packet_t *in, server_packet_t *out)
+{
+if (pid != g->current_player || g->player_status[pid] != PLAYER_ACTIVE) {
+out->packet_type = NACK; return -1;
 }
 
-void build_end_packet(game_state_t *game, player_id_t winner, server_packet_t *out) {
-    //Put state info from "game" (and calculate winner) into packet "out"
-    (void) game;
-    (void) winner;
-    (void) out;
+int cost, new_bet;
+switch (in->packet_type) {
+case CHECK:
+if (g->highest_bet != g->current_bets[pid]) { out->packet_type = NACK; return -1; }
+break;
+
+case CALL:
+if (g->highest_bet == 0 || g->current_bets[pid] == g->highest_bet) {
+    out->packet_type = NACK; return -1;
+}
+cost = g->highest_bet - g->current_bets[pid];
+if (g->player_stacks[pid] < cost) { 
+    out->packet_type = NACK; return -1; 
+}
+g->player_stacks[pid] -= cost;
+g->current_bets[pid] += cost;
+g->pot_size += cost;
+break;
+
+case RAISE:
+new_bet = in->params[0];
+if (new_bet <= g->highest_bet) { out->packet_type = NACK; return -1; }
+cost = new_bet - g->current_bets[pid];
+if (g->player_stacks[pid] < cost) { out->packet_type = NACK; return -1; }
+g->player_stacks[pid] -= cost;
+g->current_bets[pid] += cost;
+g->pot_size += cost;
+g->highest_bet = new_bet;
+break;
+
+case FOLD:
+g->player_status[pid] = PLAYER_FOLDED;
+break;
+
+default:
+out->packet_type = NACK; return -1;
+}
+
+int next = (pid + 1) % MAX_PLAYERS;
+while (next != pid) {
+if (g->player_status[next] == PLAYER_ACTIVE) break;
+next = (next + 1) % MAX_PLAYERS;
+}
+g->current_player = next;
+
+out->packet_type = ACK;
+return 0;
+}
+
+void build_info_packet(game_state_t *game, player_id_t pid, server_packet_t *out)
+{
+    memset(out, 0, sizeof(*out));
+    out->packet_type = INFO;
+    info_packet_t *p = &out->info;
+
+    p->player_cards[0] = game->player_hands[pid][0];
+    p->player_cards[1] = game->player_hands[pid][1];
+
+    for (int i = 0; i < MAX_COMMUNITY_CARDS; ++i) {
+        p->community_cards[i] = game->community_cards[i];
+    }
+    for (int i = 0; i < MAX_PLAYERS; ++i) {
+        p->player_stacks[i] = game->player_stacks[i];
+        p->player_bets[i] = game->current_bets[i];
+        p->player_status[i] = to_packet_status(game->player_status[i]);
+    }
+
+    p->pot_size = game->pot_size;
+    p->dealer = game->dealer_player;
+    p->player_turn = game->current_player;
+    p->bet_size = game->highest_bet;
+}
+
+
+void build_end_packet(game_state_t *game, player_id_t winner, server_packet_t *out)
+{
+    memset(out, 0, sizeof(*out));
+    out->packet_type = END;
+    end_packet_t *p = &out->end;
+
+    for (int pl = 0; pl < MAX_PLAYERS; ++pl) {
+        p->player_cards[pl][0] = game->player_hands[pl][0];
+        p->player_cards[pl][1] = game->player_hands[pl][1];
+    }
+
+    for (int i = 0; i < MAX_COMMUNITY_CARDS; ++i)
+        p->community_cards[i] = game->community_cards[i];
+
+    for (int i = 0; i < MAX_PLAYERS; ++i) {
+        p->player_stacks[i] = game->player_stacks[i];
+        p->player_status[i] = to_packet_status(game->player_status[i]);
+    }
+
+    p->pot_size = game->pot_size;
+    p->dealer = game->dealer_player;
+    p->winner = winner;
 }
