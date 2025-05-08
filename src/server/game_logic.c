f2eea517cec8f6886cc77e2369ea7ebd329f0b7c
Yusuf Badar <ybadar13@gmail.com>
2025-05-08 21:06:04 +0000

    WORKS

--- src/server/game_logic.c
+++ src/server/game_logic.c
@@ -31,14 +31,13 @@ void init_deck(card_t deck[DECK_SIZE], int seed){ //DO NOT TOUCH THIS FUNCTION
 
 void shuffle_deck(card_t deck[DECK_SIZE])
 {
-    for (int i = DECK_SIZE - 1; i > 0; --i) {
-        int j = rand() % (i + 1);
+    for (int i = 0; i < DECK_SIZE; ++i) {
+        int j = rand() % DECK_SIZE;
         card_t tmp = deck[i];
         deck[i]   = deck[j];
-        deck[j] = tmp;
+        deck[j]   = tmp;
     }
 }
-
 //You dont need to use this if you dont want, but we did.
 void init_game_state(game_state_t *game, int starting_stack, int random_seed)
 {
