eb30e687e1cfe59f1dbc9ce911cf4beb39b58d17
Yusuf Badar <ybadar13@gmail.com>
2025-05-09 22:54:21 +0000

    WORKS

--- src/server/poker_server.c
+++ src/server/poker_server.c
@@ -212,6 +212,12 @@ int main(int argc, char **argv)
                 if (game.player_status[s] == PLAYER_ACTIVE) { winner = s; break; }
         }
         game.player_stacks[winner] += game.pot_size;
+        for (int s = 0; s < NUM_PORTS; ++s) {
+        if (game.player_status[s] != PLAYER_LEFT) {
+            server_packet_t info;
+            build_info_packet(&game, s, &info);
+            send(game.sockets[s], &info, sizeof(info), 0);
+        }
         for (int s = 0; s < NUM_PORTS; ++s){
             if (game.player_status[s] != PLAYER_LEFT){
                 server_packet_t info;
