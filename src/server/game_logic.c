e805dd120758df4d347c6588fb4656b2d6e75fb5
Yusuf Badar <ybadar13@gmail.com>
2025-05-10 07:02:07 +0000

    PLS

--- src/server/game_logic.c
+++ src/server/game_logic.c
@@ -119,10 +119,12 @@ int server_ready(game_state_t *game) {
 
 void server_deal(game_state_t *g)
 {
-    g->round_stage = ROUND_PREFLOP;
-    g->highest_bet = 0;
+    for (int i = 0; i < 3; ++i)
+        g->community_cards[i] = g->deck[g->next_card++];
+    g->round_stage   = ROUND_FLOP;
+    g->highest_bet   = 0;
     memset(g->current_bets, 0, sizeof g->current_bets);
-    memset(has_acted, 0, sizeof has_acted);
+    memset(has_acted,       0, sizeof has_acted);
     last_raiser = -1;
 }
 
@@ -155,7 +157,7 @@ void server_community(game_state_t *g)
             g->community_cards[4] = g->deck[g->next_card++];
             g->round_stage = ROUND_RIVER;
             break;
-            
+
         case ROUND_PREFLOP:
         for (int i = 0; i < 3; i++){
             g->community_cards[i] = g->deck[g->next_card++]; 
