b5c9b7192714a9020f8ac4c2a50db1f866980cc6
Yusuf Badar <ybadar13@gmail.com>
2025-05-10 00:15:42 +0000

    PLS

--- src/server/game_logic.c
+++ src/server/game_logic.c
@@ -109,7 +109,9 @@ int server_ready(game_state_t *game) {
 
 void server_deal(game_state_t *g)
 {
-    g->round_stage = ROUND_PREFLOP;
+    for (int i = 0; i < 3; ++i)
+        g->community_cards[i] = g->deck[g->next_card++];
+    g->round_stage = ROUND_FLOP;
     g->highest_bet = 0;
     memset(g->current_bets, 0, sizeof g->current_bets);
     memset(has_acted, 0, sizeof has_acted);
