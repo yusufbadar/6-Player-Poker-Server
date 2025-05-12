e83a6432875d2d19c2dc7accf889fa1c43cd49f8
Yusuf Badar <ybadar13@gmail.com>
2025-05-12 02:33:49 +0000

    WORKS

--- src/server/client_action_handler.c
+++ src/server/client_action_handler.c
@@ -8,15 +8,16 @@
 #include "game_logic.h"
 #include "logs.h"
 
-static inline int  send_ack (server_packet_t *p) { p->packet_type = ACK;  return  0; }
-static inline int  send_nack(server_packet_t *p) { p->packet_type = NACK; return -1; }
+static inline int send_ack (server_packet_t *p) { p->packet_type = ACK;  return  0; }
+static inline int send_nack(server_packet_t *p) { p->packet_type = NACK; return -1; }
 
 static inline void maybe_allin(game_state_t *g, player_id_t pid)
 {
     if (g->player_stacks[pid] <= 0) {
         g->player_stacks[pid] = 0;
-        if (g->player_status[pid] == PLAYER_ACTIVE)
+        if (g->player_status[pid] == PLAYER_ACTIVE){
             g->player_status[pid] = PLAYER_ALLIN;
+        }
     }
 }
 
@@ -24,9 +25,9 @@ static inline uint8_t visible_status(uint8_t s)
 {
     switch (s) {
         case PLAYER_ACTIVE:
-        case PLAYER_ALLIN:  return 1;
+        case PLAYER_ALLIN: return 1;
         case PLAYER_FOLDED: return 0;
-        default:            return 2;
+        default: return 2;
     }
 }
 
@@ -46,7 +47,7 @@ int handle_client_action(game_state_t *g, player_id_t pid, const client_packet_t
             }
             g->player_stacks[pid] -= to_call;
             g->current_bets [pid] += to_call;
-            g->pot_size           += to_call;
+            g->pot_size += to_call;
             maybe_allin(g, pid);
             return send_ack(out);
 
@@ -58,7 +59,7 @@ int handle_client_action(game_state_t *g, player_id_t pid, const client_packet_t
             }
             g->player_stacks[pid] -= chips_now;
             g->current_bets [pid] += chips_now;
-            g->pot_size           += chips_now;
+            g->pot_size += chips_now;
 
             g->highest_bet = g->current_bets[pid];
             maybe_allin(g, pid);
@@ -107,31 +108,31 @@ void build_info_packet(game_state_t *g, player_id_t pid, server_packet_t *out)
 
     for (int seat = 0; seat < MAX_PLAYERS; ++seat) {
         info->player_stacks [seat] = g->player_stacks [seat];
-        info->player_bets   [seat] = g->current_bets  [seat];
+        info->player_bets [seat] = g->current_bets  [seat];
         info->player_status [seat] = visible_status(g->player_status[seat]);
     }
 
-    info->pot_size    = g->pot_size;
-    info->dealer      = g->dealer_player;
+    info->pot_size = g->pot_size;
+    info->dealer = g->dealer_player;
     info->player_turn = g->current_player;
-    info->bet_size    = g->highest_bet;
+    info->bet_size = g->highest_bet;
 }
 
 void build_end_packet(game_state_t *g, player_id_t winner, server_packet_t *out)
 {
     out->packet_type = END;
-    end_packet_t *e  = &out->end;
+    end_packet_t *e = &out->end;
 
     for (int p = 0; p < MAX_PLAYERS; ++p) {
         e->player_cards [p][0] = g->player_hands[p][0];
         e->player_cards [p][1] = g->player_hands[p][1];
-        e->player_stacks [p]   = g->player_stacks[p];
-        e->player_status[p]    = visible_status(g->player_status[p]);
+        e->player_stacks [p] = g->player_stacks[p];
+        e->player_status[p] = visible_status(g->player_status[p]);
     }
 
     memcpy(e->community_cards, g->community_cards, MAX_COMMUNITY_CARDS * sizeof(card_t));
 
     e->pot_size = g->pot_size;
-    e->dealer   = g->dealer_player;
-    e->winner   = winner;
+    e->dealer = g->dealer_player;
+    e->winner = winner;
 }
